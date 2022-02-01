8ccc23c1aec5d246334633c55e4fafda43a85585
jaewon <xldzjqpf1588@naver.com>
2022-02-01 18:21:08 +0900

    커피메이커 완료

--- src/coffeemaker/Menu.java
+++ src/coffeemaker/Menu.java
@@ -11,16 +11,14 @@ public class Menu {
 	}
 
 	// 2번
-	public int choose(String coffeeName) {
+	public MenuItem choose(String coffeeName) {
 		// 메뉴 항목을 줘야해
 		for (int i = 0; i < items.size(); i++) {
 			if (coffeeName.equals(items.get(i).getName())) {
-				return items.get(i).getPrice();
-			} else {
-				System.out.println("선택된 메뉴는 보기에 없습니다.");
+				return items.get(i);
 			}
 		}
-		return 0;
+		return null;
 	}
 
 }
--- src/data00/Test01.java
+++ src/data00/Test01.java
@@ -3,9 +3,7 @@ package data00;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
-import java.sql.ResultSet;
 import java.util.List;
 
 import com.google.gson.Gson;
